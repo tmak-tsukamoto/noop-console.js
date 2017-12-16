6980c434917b173a765633eab73082a67d86f9f7
tmak-tsukamoto <tmak.tsukamoto@gmail,com>
2017-12-16 16:18:10 +0900

    'noop-console.js' コミット
    初コミット

---
+++ noop-console.js
@@ -0,0 +1,1 @@
+(function(a){"undefined"===typeof window.console&&(window.console={assert:a,clear:a,count:a,debug:a,dir:a,dirxml:a,error:a,group:a,groupCollapsed:a,groupEnd:a,info:a,log:a,profile:a,profileEnd:a,table:a,time:a,timeEnd:a,timeStamp:a,trace:a,warn:a})})(function(){});
